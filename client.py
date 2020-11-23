910aeba10b6079c14d095184eee34219ff531541
Longfei Li <lf97310@gmail.com>
2020-11-23 19:16:57 +0800

    fix: change client ip

--- client.py
+++ client.py
@@ -2,13 +2,13 @@ import thriftpy2
 from thriftpy2.rpc import make_client
 
 exam_thrift = thriftpy2.load("./thrift_idl/exam.thrift", module_name="exam_thrift")
-exam_client = make_client(exam_thrift.ExamService, '127.0.0.1', 9091)
+exam_client = make_client(exam_thrift.ExamService, '81.68.117.198', 9091)
 
 user_thrift = thriftpy2.load("./thrift_idl/user.thrift", module_name="user_thrift")
-user_client = make_client(user_thrift.UserService, '127.0.0.1', 9092)
+user_client = make_client(user_thrift.UserService, '81.68.117.198', 9092)
 
 analysis_thrift = thriftpy2.load("./thrift_idl/analysis.thrift", module_name="analysis_thrift")
-analysis_client = make_client(analysis_thrift.AnalysisService, '127.0.0.1', 9093, timeout=10000)
+analysis_client = make_client(analysis_thrift.AnalysisService, '81.68.117.198', 9093, timeout=10000)
 
 question_thrift = thriftpy2.load("./thrift_idl/question.thrift", module_name="question_thrift")
-question_client = make_client(question_thrift.QuestionService, '127.0.0.1', 9094, timeout=10000)
+question_client = make_client(question_thrift.QuestionService, '81.68.117.198', 9094, timeout=10000)
